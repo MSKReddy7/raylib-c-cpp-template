b21cce7b28f63e8eaa3e06e257b3e3c9b0909a76
msk_reddy_chirla <mskreddy9963@gmail.com>
2026-08-01 11:20:59 +0530

    edited tasks.json and added seperate project folders

--- project/main.cpp
+++ project/main.cpp
@@ -9,7 +9,7 @@ int main(void)
     {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawText("Congrats! You created your first window! copy", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
